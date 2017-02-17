16c189676800802893d34bca1e840851f47c00b0
rk0der <ryo.alter@gmail.com>
2017-02-17 20:07:11 +0900

    print help message

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 extern crate chrono;
+extern crate getopts;
 
 use chrono::prelude::*;
+use std::{env, process};
+use getopts::Options;
+
+fn print_usage(program: &str, opts: &Options) -> () {
+  let brief = format!("Usage: {} [OPTION] [+FORMAT]", program);
+  print!("{}", opts.usage(&brief));
+  process::exit(0);
+}
+
+fn parse_args() {
+  let args: Vec<String> = env::args().collect();
+  let program = args[0].clone();
+
+  let mut opts = Options::new();
+  opts.optflag("h", "help", "print this message");
+
+  let matches = opts.parse(&args[1..])
+    .unwrap_or_else(|e| panic!(e.to_string()));
+
+  if matches.opt_present("h") {
+    print_usage(&program, &opts);
+  }
+}
 
 fn main() {
-  let local: DateTime<Local> = Local::now();
-  println!("{}", local.format("%a %b %e %T JST %Y").to_string());
+  parse_args();
+  println!("{}", Local::now().format("%a %b %e %T %Z %Y").to_string());
 }
